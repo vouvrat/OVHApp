c8c5d4eba08d48e23c559736d84f316c454294f5
vouvrat <v.ouvrat@gmail.com>
2021-08-31 22:21:35 +0200

    First commin
    premier push

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+'''
+First, install the latest release of Python wrapper: $ pip install ovh
+'''
+import json
+import ovh
+import csv
+
+# Instanciate an OVH Client.
+# You can generate new credentials with full access to your account on
+# the token creation page
+client = ovh.Client(
+    endpoint='ovh-eu',               # Endpoint of API OVH Europe (List of available endpoints)
+    application_key='xxxxxxxxxx',    # Application Key
+    application_secret='xxxxxxxxxx', # Application Secret
+    consumer_key='xxxxxxxxxx',       # Consumer Key
+)
+
+listmaillinhList = []
+
+listMailinglist = client.get('/email/domain/vouvrat.com/mailingList', 
+    #name=None, // Mailing list name (type: string)
+)
+
+result = client.get('/email/domain/vouvrat.com/mailingList/direct/subscriber', 
+    email=None, // Subscriber email (type: string)
+)
+
+
+# Pretty print
+print (json.dumps(result, indent=4))
